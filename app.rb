3a632e6dae9c9379754e6d9ba2f9858bf3786249
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-26 14:41:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Momo"
+  end
+
+  get '/hometown' do
+    "My hometown is GVA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The World is Mine by David Guetta"
+end
+
 end
