22aa332f776dce35b9bd1b2b80d173b01f1247cf
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-26 14:40:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Momo"
+  end
+
+  get '/hometown' do
+    "My hometown is GVA"
+  end
+
+  get '/song' do
+    "My favorite song is The World is Mine by David Guetta"
 end
